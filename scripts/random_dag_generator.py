902cbd2a1083affaefc8c1bd866e89f79aee5184
sunho001215 <sunho001215@gmail.com>
2022-08-26 13:08:27 +0900

    update dag visualization function

--- scripts/random_dag_generator.py
+++ scripts/random_dag_generator.py
@@ -1,6 +1,9 @@
 import random
+import os
+import plotly.graph_objects as go
 
 ############# PARAM #############
+dag_num_ = 10
 # DAG generation parameter
 depth_range_ = [2, 6]
 max_parallelism_ = 4
@@ -16,8 +19,7 @@ class Node(object):
     idx = 0
     def __init__(self):
         # Node Number
-        self.num = Node.idx
-        Node.idx += 1
+        self.idx = 1
         self.priority = 1
 
         # Execution Time
@@ -56,6 +58,7 @@ def dag_generate():
         for j in range(node_num[i]):
             node_list.append(Node())
             node_list[node_idx].level = i + 1
+            node_list[node_idx].idx = j + 1
             if i == 0:
                 node_list[node_idx].parent.append(0)
             else:
@@ -96,10 +99,6 @@ def dag_generate():
                 else:
                     break
 
-    for i in range(node_idx):
-        print("node_idx: "+str(i)+", level: "+str(node_list[i].level)+", parent: "+str(node_list[i].parent)+", child: "+str(node_list[i].child))
-    print(node_num)
-
     return node_list
 
 def dag_check(node_list):
@@ -114,10 +113,70 @@ def dag_check(node_list):
             assert((i in node_list[parent_idx].child))
             assert((parent_idx < i))
 
+def graph_visualize(node_list, html_path):
+    edge_x, edge_y = [], []
+    node_x, node_y = [], []
+    node_text = []
+    for node in node_list:
+        # add node
+        node_x.append(node.level)
+        node_y.append(node.idx)
+        node_text.append('Default Execution Time: ' + str(node.d_exec_t) + ", Callback Execution Time: " + str(node.c_exec_t))
+        # add edge
+        for child_node in node.child:
+            edge_x.append(node.level)
+            edge_x.append(node_list[child_node].level)
+            edge_x.append(None)
+            edge_y.append(node.idx)
+            edge_y.append(node_list[child_node].idx)
+            edge_y.append(None)
+    
+    edge_trace = go.Scatter(
+    x=edge_x, y=edge_y,
+    line=dict(width=0.5, color='#888'),
+    hoverinfo='none',
+    mode='lines')
+
+    node_trace = go.Scatter(
+    x=node_x, y=node_y,
+    mode='markers',
+    hoverinfo='text',
+    marker=dict(
+        showscale=False,
+        colorscale='YlGnBu',
+        reversescale=False,
+        color=[],
+        size=30,
+        line_width=2))
+    
+    node_trace.text = node_text
+    
+    fig = go.Figure(data=[edge_trace, node_trace],
+                layout=go.Layout(
+                showlegend=False,
+                hovermode='closest',
+                margin=dict(b=20,l=5,r=5,t=40),
+                annotations=[ dict(
+                    showarrow=False,
+                    x=0.005, y=-0.002 ) ],
+                xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                )
+    fig.write_html(html_path)
+
 def execution_time_assign(node_list):
     return node_list
 
 if __name__ == "__main__":
-    node_list = dag_generate()
-    dag_check(node_list)
-    node_list = execution_time_assign(node_list)
+    parent_dir = os.getcwd()
+    html_dir = parent_dir + '/html/'
+    csv_dir = parent_dir + '/csv/raw_data/'
+    for i in range(dag_num_):
+        name = "DAG_" + str(i+1).zfill(3)
+        html_path = html_dir + name + '.html'
+        csv_path = csv_dir + name + '.csv'
+
+        node_list = dag_generate()
+        dag_check(node_list)
+        node_list = execution_time_assign(node_list)
+        graph_visualize(node_list, html_path)
